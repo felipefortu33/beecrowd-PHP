700fdfa2a63ae7e69dda1c3d5845cc5126ae7462
Felipefortu33 <felipefortu33@gmail.com>
2024-07-04 10:33:54 -0300

    Resolução Beecrowd 1007

---
+++ bee1007.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = readline();
+$b = readline();
+$c = readline();
+$d = readline();
+
+$DIFERENCA = ($a * $b - $c * $d);
+
+echo"DIFERENCA = ". $DIFERENCA . PHP_EOL;
+
+//link do problema no beecrowd https://judge.beecrowd.com/pt/problems/view/1007
