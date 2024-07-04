39f05ac1fc134c2f0fec7e6093e78bf337774f13
Felipefortu33 <felipefortu33@gmail.com>
2024-07-04 10:26:25 -0300

    Resolução Beecrowd 1004

---
+++ bee1004.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = readline();
+$b = readline();
+
+$PROD = $a * $b;
+
+echo"PROD = " . $PROD . PHP_EOL;
+
+//link do problema no beecrowd https://judge.beecrowd.com/pt/problems/view/1003
