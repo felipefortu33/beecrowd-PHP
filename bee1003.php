d4005863cb11ad43283632dd92f84bea1653bb41
Felipefortu33 <felipefortu33@gmail.com>
2024-07-04 10:13:35 -0300

    Resolução Beecrowd 1003

---
+++ bee1003.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = readline();
+$b = readline();
+
+$SOMA = $a + $b;
+
+echo"SOMA = " . $SOMA . PHP_EOL;
+
+//link do problema no beecrowd https://judge.beecrowd.com/pt/problems/view/1003
