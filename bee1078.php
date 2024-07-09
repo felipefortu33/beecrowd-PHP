899540a3bf86b549e82692bc127f219975656115
Felipefortu33 <felipefortu33@gmail.com>
2024-07-09 08:27:25 -0300

    Resolução Beecrowd 1078

---
+++ bee1078.php
@@ -0,0 +1,6 @@
+<?php
+
+$N = readline();
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . " x " . $N . " = " . $i * $N . "\n"; //forma para calcular tabuada pega o numero digitado e multiplica de 1 a 10.
+}
