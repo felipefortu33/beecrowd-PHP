496fb996d0c8ac0ffe94d03a228449cf4a8a0b07
Felipefortu33 <felipefortu33@gmail.com>
2024-07-09 08:28:59 -0300

    Resolução Beecrowd 1096

---
+++ bee1096.php
@@ -0,0 +1,11 @@
+
+<?php
+
+// Loop para variar o valor de I de 1 a 9
+for ($i = 1; $i <= 9; $i += 2) {
+    // Loop para variar o valor de J de 7 a 5 em ordem decrescente
+    for ($j = 7; $j >= 5; $j--) {
+        // Impressão da sequência no formato especificado
+        echo "I=$i J=$j\n";
+    }
+}
