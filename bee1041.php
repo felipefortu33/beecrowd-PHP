9fceb8a4f60df535dfd3119ad80f4776514aded8
Felipefortu33 <felipefortu33@gmail.com>
2024-07-09 08:15:07 -0300

    Resolução Beecrowd 1041

---
+++ bee1041.php
@@ -0,0 +1,22 @@
+<?php
+
+$valores = explode(" ", readline());
+
+$x = $valores[0];
+$y = $valores[1];
+
+if ($x == 0 && $y == 0) {
+    echo "Origem\n";
+} elseif ($x == 0) {
+    echo "Eixo Y\n";
+} elseif ($y == 0) {
+    echo "Eixo X\n";
+} elseif ($x > 0 && $y > 0) {
+    echo "Q1\n";
+} elseif ($x < 0 && $y > 0) {
+    echo "Q2\n";
+} elseif ($x < 0 && $y < 0) {
+    echo "Q3\n";
+} else {
+    echo "Q4\n";
+}
